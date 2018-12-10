829e5f15373c382ccea2af133da3ad263251cfe5
Lina Tymoshenko <linochkato@gmail.com>
2018-12-11 00:03:43 +0200

    get weather by country using XHR

--- xhr-weather/index.js
+++ xhr-weather/index.js
@@ -1,3 +1,8 @@
+const DEFAULT_COUNTRY = "Украина";
+const API_KEY = "1914cd5f635de9246955aa90978ff6e8";
+const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const API_IMAGE_URL = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/";
+
 function httpGet(url) {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
@@ -22,13 +27,28 @@ function httpGet(url) {
   });
 }
 
-function sendRequest(country = "Ukraine") {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=1914cd5f635de9246955aa90978ff6e8`;
+function sendRequest(country = DEFAULT_COUNTRY) {
+  const url = `${API_URL}?q=${country}&units=metric&appid=${API_KEY}`;
 
   return httpGet(url);
 }
 
-sendRequest().then((response) => {
-  let c;
-  //openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/03n.png
-});
+function getWeatherForCountry() {
+  let country = document.getElementById("search-country").value;
+
+  sendRequest(country)
+    .then(({main: {temp}, weather: [{icon, description}]}) => {
+      document.getElementById("country").innerHTML = country;
+      document.getElementById("temperature").innerHTML = temp;
+      let imageBlock = document.getElementById("image-block");
+
+      while (imageBlock.firstChild) {
+        imageBlock.removeChild(imageBlock.firstChild);
+      }
+
+      let weatherImage = document.createElement("IMG");
+      weatherImage.src = `${API_IMAGE_URL}${icon}.png`;
+      weatherImage.alt = description;
+      imageBlock.appendChild(weatherImage);
+    });
+}
