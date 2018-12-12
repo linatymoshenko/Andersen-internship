2eda79e76f37dc41a6679dfd4f0b1250ecf9683c
Lina Tymoshenko <linochkato@gmail.com>
2018-12-12 13:34:24 +0200

    code reformat

--- examples/camelizeString.js
+++ examples/camelizeString.js
@@ -1,13 +1,13 @@
 function camelize(string) {
-  function camelize(string) {
     let array = string.split("-");
 
     for (let index = 1; index < array.length; index++) {
       array[index] = array[index].charAt(0).toUpperCase() + array[index].slice(1);
     }
-    return array.join("")
-  }
 
-camelize("background-color");  // 'backgroundColor';
-camelize("list-style-image");  //'listStyleImage';
-camelize("-webkit-transition"); //'WebkitTransition';
+    return array.join("")
+}
+
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
