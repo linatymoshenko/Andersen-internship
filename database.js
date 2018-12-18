679baddccc1e3fd5ef95e57af496998f6ff4e09f
Lina Tymoshenko <linochkato@gmail.com>
2018-12-18 22:38:52 +0200

    create database and server to get and add cities

---
+++ database.js
@@ -0,0 +1,29 @@
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(":memory:");
+
+class Database {
+  constructor() {
+    db.serialize(function() {
+        db.run("CREATE TABLE cities (name TEXT)");
+       
+        var stmt = db.prepare("INSERT INTO cities VALUES (?)");
+        stmt.run("Kiev");
+        stmt.run("Moscow");
+        stmt.run("Cherkassy");
+        stmt.run("Paris");
+        stmt.run("London");
+        stmt.finalize(); 
+      });
+  } 
+  getCity(callback) {
+    db.get("SELECT * FROM cities ORDER BY RANDOM() LIMIT 1;", callback);
+  }
+  setCity(name) {
+    db.run("INSERT INTO cities VALUES (?)", name);
+  }
+  close() {
+      db.close();
+  }
+}
+
+module.exports = Database;
