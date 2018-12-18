3fc6ed1514deb6adeb8889a5d37297b443fc4fa8
Lina Tymoshenko <linochkato@gmail.com>
2018-12-18 22:53:50 +0200

    code reformat

--- database.js
+++ database.js
@@ -1,28 +1,31 @@
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database(":memory:");
+let sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database(":memory:");
 
 class Database {
   constructor() {
-    db.serialize(function() {
-        db.run("CREATE TABLE cities (name TEXT)");
-       
-        var stmt = db.prepare("INSERT INTO cities VALUES (?)");
-        stmt.run("Kiev");
-        stmt.run("Moscow");
-        stmt.run("Cherkassy");
-        stmt.run("Paris");
-        stmt.run("London");
-        stmt.finalize(); 
-      });
-  } 
+    db.serialize(function () {
+      db.run("CREATE TABLE cities (name TEXT)");
+
+      let stmt = db.prepare("INSERT INTO cities VALUES (?)");
+      stmt.run("Kiev");
+      stmt.run("Moscow");
+      stmt.run("Cherkassy");
+      stmt.run("Paris");
+      stmt.run("London");
+      stmt.finalize();
+    });
+  }
+
   getCity(callback) {
     db.get("SELECT * FROM cities ORDER BY RANDOM() LIMIT 1;", callback);
   }
+
   setCity(name) {
     db.run("INSERT INTO cities VALUES (?)", name);
   }
+
   close() {
-      db.close();
+    db.close();
   }
 }
 
--- server.js
+++ server.js
@@ -1,47 +1,37 @@
-const { createServer } = require("http")
-const { parse } = require("url")
-const next = require("next")
-const Database = require("./database")
+const {createServer} = require("http");
+const {parse} = require("url");
+const next = require("next");
+const Database = require("./database");
 const db = new Database();
 
-const dev = process.env.NODE_ENV !== "production"
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({dev});
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+    const parsedUrl = parse(req.url, true);
+    const {pathname, query} = parsedUrl;
 
     if (pathname === "/api/city/random") {
-        db.getCity((err, row) => {
-            res.end(row.name)
-        });
+      db.getCity((err, row) => {
+        res.end(row.name)
+      });
+    } else if (pathname === "/api/city/add") {
+      req.on("error", (err) => {
+        console.error(err);
+      }).on("data", (chunk) => {
+        let jsonString = chunk.toString();
+        let body = JSON.parse(jsonString);
+        db.setCity(body.name);
+      });
+
+      res.end("succes");
+    } else {
+      handle(req, res, parsedUrl)
     }
-
-    if (pathname === "/api/city/add") {
-        req.on("error", (err) => {
-            console.error(err);
-          }).on("data", (chunk) => {
-            let jsonString = chunk.toString();
-            let body = JSON.parse(jsonString);
-            db.setCity(body.name);
-          })
-
-        res.end("succes");
-    }
-
-    // if (pathname === '/a') {
-    //   app.render(req, res, '/b', query)
-    // } else if (pathname === '/b') {
-    //   app.render(req, res, '/a', query)
-    // } else {
-    //   handle(req, res, parsedUrl)
-    // }
   }).listen(3000, err => {
-    if (err) throw err
+    if (err) throw err;
     console.log("> Ready on http://localhost:3000")
   })
-})
+});
